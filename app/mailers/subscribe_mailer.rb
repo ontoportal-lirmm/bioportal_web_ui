3f094510271d094540995585a87d228213dd5462
HADDAD Zineddine <hz_haddad@esi.dz>
2023-01-09 19:20:32 +0100

    fix & update

--- app/mailers/subscribe_mailer.rb
+++ app/mailers/subscribe_mailer.rb
@@ -1,28 +1,32 @@
 class SubscribeMailer < ApplicationMailer
     
     def register_for_announce_list(email,firstName,lastName)
-      if are_subscription_configs_valid?
-        mail(
-          :to => $ANNOUNCE_SERVICE_HOST, 
-          :from => email, 
-          :subject => "subscribe #{$ANNOUNCE_LIST} #{firstName} #{lastName}") 
-        end  
+      if subscription_configs_valid?
+        if $ANNOUNCE_LIST_SERVICE.upcase.eql? "SYMPA"
+          mail(
+            :to => $ANNOUNCE_SERVICE_HOST, 
+            :from => email, 
+            :subject => "subscribe #{$ANNOUNCE_LIST} #{firstName} #{lastName}") 
+          end  
+        end
       end
        
     def unregister_for_announce_list(email)
-      if are_subscription_configs_valid?
-        mail(
-          :to => $ANNOUNCE_SERVICE_HOST, 
-          :from => email, 
-          :subject => "unsubscribe #{$ANNOUNCE_LIST}")    
+      if subscription_configs_valid?
+        if $ANNOUNCE_LIST_SERVICE.upcase.eql? "SYMPA"
+          mail(
+            :to => $ANNOUNCE_SERVICE_HOST, 
+            :from => email, 
+            :subject => "unsubscribe #{$ANNOUNCE_LIST}")
+        end
       end  
     end
     
 
     private
 
-    def are_subscription_configs_valid?
-      $ANNOUNCE_LIST.present? &&  $ANNOUNCE_LIST_SERVICE.present? && $ANNOUNCE_LIST.present? 
+    def subscription_configs_valid?
+      $ANNOUNCE_SERVICE_HOST.present? &&  $ANNOUNCE_LIST_SERVICE.present? && $ANNOUNCE_LIST.present? 
     end
     
 end
