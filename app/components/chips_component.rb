0a08cdf224bc1b71f41e8fd09a97b99329c9c3d7
Bilelkihal <61744974+Bilelkihal@users.noreply.github.com>
2023-02-03 14:26:26 +0100

    Add a function to turn spaces into underscores

--- app/components/chips_component.rb
+++ app/components/chips_component.rb
@@ -2,4 +2,8 @@ class ChipsComponent < ViewComponent::Base
     def initialize(text:)
         @text = text
     end
+
+    def text_underscore
+        @text.parameterize(separator: '_')
+    end
 end
