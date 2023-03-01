513cd53fcdecf5e728981d3fc48a71e5970116ac
Raimi Solorzano Niederhausen <raimi.solorzano@gmail.com>
2023-03-01 18:10:03 +0100

    fix condition for no_appliance_id_for_comparison

--- app/validators/virtual_appliance_id_validator.rb
+++ app/validators/virtual_appliance_id_validator.rb
@@ -1,7 +1,7 @@
 # VirtualApplianceIdValidator
 #
-# Custom validator for virtual appliance IDs. The virtual appliance ID in a 
-# license key must match the ID of the virtual appliance against which a 
+# Custom validator for virtual appliance IDs. The virtual appliance ID in a
+# license key must match the ID of the virtual appliance against which a
 # license is submitted.
 #
 #   class License < ApplicationRecord
@@ -17,11 +17,12 @@ class VirtualApplianceIdValidator < ActiveModel::EachValidator
     response = JSON.parse(
       LinkedData::Client::HTTP.get(LinkedData::Client.settings.rest_url + "/admin/update_info", {}, raw: true)
     )
-    if response["error"]
+
+    if response["appliance_id"].nil?
       record.errors.add(attribute, :no_appliance_id_for_comparison)
       return false
     end
-    
+
     unless valid_virtual_appliance_id?(value, response["appliance_id"])
       record.errors.add(attribute, :appliance_id_mismatch)
     end
