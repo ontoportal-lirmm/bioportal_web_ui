c57eb9faf3dcc71b0b60d3c3da72cc439c1616e1
Bilelkihal <61744974+Bilelkihal@users.noreply.github.com>
2023-03-27 16:20:28 +0200

    Add the checked property to the chips component

--- app/components/chips_component.rb
+++ app/components/chips_component.rb
@@ -1,6 +1,11 @@
 class ChipsComponent < ViewComponent::Base
-    def initialize(name:, value:)
+    def initialize(name:, value:, checked: false)
         @name = name
         @value = value
+        @checked = checked
+    end
+
+    def checked?
+        @checked
     end
 end
