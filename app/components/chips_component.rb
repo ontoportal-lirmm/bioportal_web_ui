0efa40389703e1512291289d569248d820a06661
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-05-06 19:47:19 +0200

    update chips component  to add label attribute

--- app/components/chips_component.rb
+++ app/components/chips_component.rb
@@ -1,9 +1,10 @@
 class ChipsComponent < ViewComponent::Base
     renders_one :count
-    def initialize(name:, value:, checked: false)
+    def initialize(name:, value:, label: nil,checked: false)
         @name = name
         @value = value
         @checked = checked
+        @label = label || @value
     end
 
     def checked?
