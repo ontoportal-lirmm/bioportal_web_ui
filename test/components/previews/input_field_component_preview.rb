ca8c459843ff4abcf1eb0379bd48ed8faa3419b3
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2023-06-30 12:37:27 +0000

    add date field to the lookbook

--- test/components/previews/input_field_component_preview.rb
+++ test/components/previews/input_field_component_preview.rb
@@ -5,5 +5,12 @@ class InputFieldComponentPreview < ViewComponent::Preview
     def default(label: "Label")
         render InputFieldComponent.new(label: label, name: "name", type: "text", width: "100%", margin_bottom: "0")
     end
-  
-end
+
+    # @param label text   
+
+    def date(label: "Label")
+        render InputFieldComponent.new(label: label, name: "name", type: "text", width: "100%", margin_bottom: "0", type: "date")
+    end
+
+
+end 
