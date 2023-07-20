fdb1a250fb7612c8683ef09b12c4c0f7dc1e484c
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2023-07-20 15:58:42 +0000

    tmp

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -58,8 +58,8 @@ application.register("skos-collection-colors", SkosCollectionColorsController)
 import TextTruncateController from "./text_truncate_controller"
 application.register("text-truncate", TextTruncateController)
 
-import Ontology_viewer_tabs_controller from "./ontology_viewer_tabs_controller"
-application.register("ontology-viewer-tabs", Ontology_viewer_tabs_controller)
+import TomSelectController from "./tom_select_controller"
+application.register("tom-select", TomSelectController)
 
 import TooltipController from "./tooltip_controller"
 application.register("tooltip", TooltipController)
