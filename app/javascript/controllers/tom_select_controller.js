8d6f358eca72d0cd2ea86a23f1fd530ea152b7b8
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2023-07-27 21:36:39 +0000

    tmp

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -3,10 +3,13 @@ import TomSelect from "tom-select"
 // Connects to data-controller="tom-select"
 export default class extends Controller {
 
+  
+
   connect() {
+    
     new TomSelect(this.element, {
       
-
+      
       //plugins: ['remove_button']
     });
   }
