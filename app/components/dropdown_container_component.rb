0391943e04080d27ce2614c9453833d8b3e9169a
DJEBABRIA Hiba <ih_djebabria@esi.dz>
2023-07-31 10:04:26 +0200

    add the tooltip to the dropwon container

--- app/components/dropdown_container_component.rb
+++ app/components/dropdown_container_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class DropdownContainerComponent < ViewComponent::Base
-
-  def initialize(title:, id:)
+  include ApplicationHelper
+  def initialize(title:, id:, tooltip:nil)
     super
     @title = title
     @id = id
+    @tooltip = tooltip
   end
 end
