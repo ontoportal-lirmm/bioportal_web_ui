6526367c746060018da12984e21bd97916acec04
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-08-08 11:49:49 +0200

    add disable state to input field component

--- app/components/input/text_input_component.rb
+++ app/components/input/text_input_component.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class Input::TextInputComponent < Input::InputFieldComponent
-  def initialize(label: '', name:, value: nil, placeholder: '', error_message: '', helper_text: '')
-    super(label: label, name: name, value: value,  placeholder: placeholder, error_message: error_message, helper_text: helper_text)
+  def initialize(label: '', name:, value: nil, placeholder: '', error_message: '', helper_text: '', disabled: false)
+    super(label: label, name: name, value: value,  placeholder: placeholder, error_message: error_message, helper_text: helper_text, disabled: disabled)
   end
 
   def call
-    render Input::InputFieldComponent.new(label: @label, name: @name, value: @value,  placeholder: @placeholder, error_message: @error_message, helper_text: @helper_text, type: @type)
+    render Input::InputFieldComponent.new(label: @label, name: @name, value: @value,  placeholder: @placeholder,
+                                          error_message: @error_message, helper_text: @helper_text,
+                                          type: @type, disabled: @disabled)
   end
 end
