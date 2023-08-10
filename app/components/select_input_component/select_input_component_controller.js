7eabe5a6d96cc28685ed47bfe3600c492fc91dff
HADDAD Zineddine <hz_haddad@esi.dz>
2023-08-10 14:14:55 +0000

    use tomselect

--- app/components/select_input_component/select_input_component_controller.js
+++ app/components/select_input_component/select_input_component_controller.js
@@ -4,36 +4,38 @@ import TomSelect from "tom-select"
 export default class extends Controller {
     static values = {
         multiple: { type: Boolean, default: false },
-        openAdd : { type: Boolean, default: false }.
+        openAdd : { type: Boolean, default: false },
         withIcon : { type: Boolean, default: false },
         valueField: { type: String, default: 'id' },
         searchField: { type: String, default: 'title' },
         options: { type: Array, default: [] }
-    }
+    };
 
 
     connect() {
         let myOptions = {}
+
         
         if (this.withIconValue) {
-
             myOptions = {
                 valueField: this.valueFieldValue,
-                searchField: this.searchFieldvalue,
+                searchField: this.searchFieldValue,
                 options: this.optionsValue,
                 render: {
                     option: (data, escape) => {
-                        return '<div>' +
-                                '<span class="title">' + escape(data.title) + '</span>' +
-                                '<span class="url">' + escape(data.url) + '</span>' +
-                            '</div>';
+                        return `<div>
+                                <span class="${escape(data.icon)}"></span>
+                                <span class="title ml-2">${escape(data.title)}</span>  
+                                </div>`;
                     },
                     item: (data, escape) => {
-                        return '<div title="' + escape(data.url) + '">' + escape(data.title) + '</div>';
+                        return `<div title="${escape(data.id)}">
+                                <span class="${escape(data.icon)}"></span>
+                                <span class="title ml-2">${escape(data.title)}</span>  
+                            </div>`;
                     }
                 }
             }
-
 
 
         } else {
