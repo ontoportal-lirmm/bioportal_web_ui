e408c01c770154c413849ba35a57b6742d0eefce
HADDAD Zineddine <hz_haddad@esi.dz>
2023-08-10 13:25:36 +0000

    customizing TomSelect controller

--- app/components/select_input_component/select_input_component_controller.js
+++ app/components/select_input_component/select_input_component_controller.js
@@ -3,17 +3,52 @@ import TomSelect from "tom-select"
 
 export default class extends Controller {
     static values = {
-        multiple: Boolean,
-        openAdd : Boolean
+        multiple: { type: Boolean, default: false },
+        openAdd : { type: Boolean, default: false }.
+        withIcon : { type: Boolean, default: false },
+        valueField: { type: String, default: 'id' },
+        searchField: { type: String, default: 'title' },
+        options: { type: Array, default: [] }
     }
+
+
     connect() {
         let myOptions = {}
-        if (this.multipleValue) {
-            myOptions['plugins'] = ['remove_button'];
+        
+        if (this.withIconValue) {
+
+            myOptions = {
+                valueField: this.valueFieldValue,
+                searchField: this.searchFieldvalue,
+                options: this.optionsValue,
+                render: {
+                    option: (data, escape) => {
+                        return '<div>' +
+                                '<span class="title">' + escape(data.title) + '</span>' +
+                                '<span class="url">' + escape(data.url) + '</span>' +
+                            '</div>';
+                    },
+                    item: (data, escape) => {
+                        return '<div title="' + escape(data.url) + '">' + escape(data.title) + '</div>';
+                    }
+                }
+            }
+
+
+
+        } else {
+            if (this.multipleValue) {
+                myOptions['plugins'] = ['remove_button'];
+            }
+            if (this.openAddValue) {
+                myOptions['create'] = true;
+            }
         }
-        if (this.openAddValue) {
-            myOptions['create'] = true;
-        }
+        
+        
+        
+        
+       
         new TomSelect(this.element, myOptions);
     }
 
