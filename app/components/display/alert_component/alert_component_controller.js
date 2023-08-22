8fd091c49f5bb65171aa810bfd2c65154d62a58d
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2023-08-22 16:56:22 +0000

    make alert component get closed auto after a delai

--- app/components/display/alert_component/alert_component_controller.js
+++ app/components/display/alert_component/alert_component_controller.js
@@ -1,6 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  connect(){
+    setTimeout(() => {
+      this.element.style.display = "none";
+    }, 5000);
+  }
   close(){
     this.element.style.display = "none"
   }
