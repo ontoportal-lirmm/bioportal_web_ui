485b7964d9ce2bb5f16cef3d834ac30e0b068051
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-08-29 17:28:00 +0200

    add data attribute for select and text  components

--- app/components/input/select_component.rb
+++ app/components/input/select_component.rb
@@ -2,8 +2,8 @@
 
 class Input::SelectComponent < Input::InputFieldComponent
 
-  def initialize(id: nil, label: '', name:, value: [], selected: '', placeholder: '', error_message: '', helper_text: '', multiple: false, open_to_add_values: false)
-    super(label: label, name: name, value: value,  placeholder: placeholder, error_message: error_message, helper_text: helper_text)
+  def initialize(id: nil, label: '', name:, value: [], selected: '', placeholder: '', error_message: '', helper_text: '', multiple: false, open_to_add_values: false, data: {})
+    super(label: label, name: name, value: value,  placeholder: placeholder, error_message: error_message, helper_text: helper_text, data: data)
     @values = value
     @selected = selected
     @open_to_add_values = open_to_add_values
