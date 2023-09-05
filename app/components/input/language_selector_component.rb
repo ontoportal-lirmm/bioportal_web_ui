40d9bf241f2ddcafa76fe5c81805facccf2f4faa
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-09-05 02:04:57 +0200

    add language selector component

---
+++ app/components/input/language_selector_component.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Input::LanguageSelectorComponent < ViewComponent::Base
+
+  def initialize(languages:, id: '', name: '' )
+    super
+    @languages = languages
+    @id = id
+    @name = languages
+  end
+
+  def languages_options
+    values = [['All languages', 'all']]
+
+    @languages.each do |key, label|
+      option = "<div>#{render(LanguageFieldComponent.new(value: key.to_s.downcase, label: label))}</div>"
+      values += [[option, key.to_s.downcase]]
+    end
+    values
+  end
+
+  def call
+    render SelectInputComponent.new(id: @id, name: @name, values: languages_options, placeholder: 'Select a language')
+  end
+end
