277dabf2d28d0a9cce363106ab8f86e4ad69b33f
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-09-05 06:17:34 +0200

    Merge pull request #238 from ontoportal-lirmm/feature/update-nav-bar-design
    Feature: Update nav bar design

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -52,6 +52,9 @@ application.register("ontology-viewer-tabs", Ontology_viewer_tabs_controller)
 import TooltipController from "./tooltip_controller"
 application.register("tooltip", TooltipController)
 
+import TopnavResponsivenessController from "./topnav_responsiveness_controller"
+application.register("topnav-responsiveness", TopnavResponsivenessController)
+
 import TurboFrameController from "./turbo_frame_controller"
 application.register("turbo-frame", TurboFrameController)
 
