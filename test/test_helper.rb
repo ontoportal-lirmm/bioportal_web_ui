9ee25574d83b4fb56f729c3e3dbca3c2996561c6
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-11-19 23:47:30 +0100

    add users creation & login  flow tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,5 +11,26 @@ class ActiveSupport::TestCase
 
   Capybara.server_host = "0.0.0.0"
   Capybara.app_host = "http://#{Socket.gethostname}:#{Capybara.server_port}"
+end
 
+# Define the fixtures helper method
+def fixtures(fixture_name)
+  @global_fixtures ||= load_all_fixtures
+  @global_fixtures[fixture_name.to_s]
+end
+
+# Load all fixtures method
+def load_all_fixtures
+  fixtures_directory = Rails.root.join('test', 'fixtures')
+  fixture_files = Dir.glob(File.join(fixtures_directory, '*.yml'))
+
+  fixtures_data = {}
+
+  fixture_files.each do |fixture_file|
+    fixture_name = File.basename(fixture_file, '.yml')
+    data = YAML.load_file(fixture_file)
+    fixtures_data[fixture_name] = OpenStruct.new(Array(data).map{|key, hash| [key , OpenStruct.new(hash)]}.to_h)
+  end
+
+  fixtures_data
 end
