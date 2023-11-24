5f47cd64edd9f06306b0ac5ec2d500616205d8eb
Syphax bouazzouni <gs_bouazzouni@esi.dz>
2023-11-24 01:51:54 +0100

    fix subscribe button unwatch state title

--- app/components/ontology_subscribe_button_component.rb
+++ app/components/ontology_subscribe_button_component.rb
@@ -3,6 +3,7 @@
 class OntologySubscribeButtonComponent < ViewComponent::Base
   def initialize(ontology_id:, subscribed:, user_id:, count: 0, link: 'javascript:void(0);')
     super
+    @subscribed = subscribed
     @sub_text = subscribed ? 'UnWatch' : 'Watch'
     @link = link
     @count = count
@@ -14,12 +15,14 @@ class OntologySubscribeButtonComponent < ViewComponent::Base
         'subscribe-notes-user-id-value': user_id,
         action: 'click->subscribe-notes#subscribeToNotes',
       },
-      title:  title
+      title: title
     }
   end
 
   def title
-    if @count.zero?
+    if @subscribed
+      "#{@sub_text} this resource"
+    elsif @count.zero?
       "Be the first to watch this resource and  be notified of all its updates"
     else
       "Join the #{@count} users, watching this resource and  be notified of all its updates"
