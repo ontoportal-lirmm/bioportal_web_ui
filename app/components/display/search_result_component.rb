f12ce21ba9aa4ae837a916ee3e8306134129b855
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-12-24 21:55:26 +0100

    simplify search result component by inferring some arguments

--- app/components/display/search_result_component.rb
+++ app/components/display/search_result_component.rb
@@ -3,15 +3,13 @@ class Display::SearchResultComponent < ViewComponent::Base
     include ModalHelper
     renders_many :subresults, Display::SearchResultComponent
     renders_many :reuses, Display::SearchResultComponent
-    def initialize(title: nil, uri: nil, definition: nil, link: nil, other_reuses: nil, is_sub_component: false, sub_number: 0, reuses: 0)
+    def initialize(title: nil, ontology_acronym: nil ,uri: nil, definition: nil, link: nil,  is_sub_component: false)
         @title = title
         @uri = uri
         @definition = definition
         @link = link
-        @other_reuses = other_reuses
         @is_sub_component = is_sub_component
-        @sub_number = sub_number
-        @reuses = reuses
+        @ontology_acronym = ontology_acronym
     end
 
     def sub_component_class
