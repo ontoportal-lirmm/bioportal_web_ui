1bca819877792da760882a073713f34c77e5f7b4
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2023-12-26 09:56:34 +0000

    extract details and vizualize buttons to functions, in search result component

--- app/components/display/search_result_component.rb
+++ app/components/display/search_result_component.rb
@@ -15,4 +15,25 @@ class Display::SearchResultComponent < ViewComponent::Base
     def sub_component_class
         @is_sub_component ? 'sub-component' : ''
     end
+
+    def details_button
+        link_to_modal(nil, "/ajax/class_details?modal=true&ontology=#{@ontology_acronym}&conceptid=#{@uri}&styled=false", data: { show_modal_title_value: @title, show_modal_size_value: 'modal-xl' }) do
+          content_tag(:div, class: 'button') do
+            concat inline_svg_tag('icons/details.svg')
+            concat content_tag(:div, class: 'text') { 'Details' }
+          end
+        end
+    end
+
+    def visualize_button
+        link_to_modal(nil, "/ajax/biomixer/?ontology=#{@ontology_acronym}&conceptid=#{@uri}", data: { show_modal_title_value: @title, show_modal_size_value: 'modal-xl' }) do
+          content_tag(:div, class: 'button') do
+            concat inline_svg_tag('icons/visualize.svg')
+            concat content_tag(:div, class: 'text') { 'Visualize' }
+          end
+        end
+    end
+      
+      
+      
 end
