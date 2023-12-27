4f131b2346ce464d64690699639c4935c7e7c65c
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2023-12-27 23:18:38 +0000

    Clean search result component stimulus controller code

--- app/components/display/search_result_component/search_result_component_controller.js
+++ app/components/display/search_result_component/search_result_component_controller.js
@@ -3,17 +3,9 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = [ "subOntologies" , "reuses" ]
   showSubOntologies(){
-    if(this.subOntologiesTarget.classList.contains('d-none')){
-        this.subOntologiesTarget.classList.remove("d-none")
-    } else {
-        this.subOntologiesTarget.classList.add("d-none")
-    }
+    this.subOntologiesTarget.classList.toggle("d-none")
   }
   showReuses(){
-    if(this.reusesTarget.classList.contains('d-none')){
-      this.reusesTarget.classList.remove("d-none")
-    } else {
-        this.reusesTarget.classList.add("d-none")
-    }
+    this.reusesTarget.classList.toggle("d-none")
   }
 }
