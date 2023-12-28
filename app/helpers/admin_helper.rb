2cdc08a125edfe8eb74b53cb3356c0566bf79be7
Syphax bouazzouni <gs_bouazzouni@esi.dz>
2023-12-28 17:37:52 +0100

    add Analytics section to the admin page

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -3,4 +3,11 @@ module AdminHelper
     current_section = params[:section] || 'site'
     current_section.eql?(section_title)
   end
+
+
+  def new_ontologies_created_title
+    content_tag(:div,
+                "The following ontologies: #{@new_ontologies_count.join(', ')} were created in this year",
+                style: 'width: 400px; max-height: 300px')
+  end
 end
