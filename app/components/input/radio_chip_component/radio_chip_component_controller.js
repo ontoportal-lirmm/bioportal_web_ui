c02398aca193cd97f5e78ef13417bee26ba87e23
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2024-01-04 14:56:00 +0000

    make radio chip selected initially

--- app/components/input/radio_chip_component/radio_chip_component_controller.js
+++ app/components/input/radio_chip_component/radio_chip_component_controller.js
@@ -2,10 +2,14 @@ import {Controller} from "@hotwired/stimulus";
 
 
 export default class extends Controller {
+    connect(){
+        if(this.element.children[0].checked){
+            this.element.classList.add('selected')
+        }
+    }
     check(){
         if(this.element.children[0].checked){
             this.element.classList.add('selected')
-            
         }
         let other_radios = document.getElementsByClassName(this.element.children[0].name)
         let element = this.element
@@ -14,7 +18,5 @@ export default class extends Controller {
               otherRadio.classList.remove('selected')
             }
         });
-
-        
     }
 }
