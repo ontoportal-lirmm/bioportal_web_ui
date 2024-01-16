23ea7b9ee29ecefaefa5c2a47a4bb38e26d2a1b4
Syphax bouazzouni <gs_bouazzouni@esi.dz>
2024-01-16 02:00:19 +0100

    update reveal component to handle multiple containers

--- app/components/layout/reveal_component/reveal_component_controller.js
+++ app/components/layout/reveal_component/reveal_component_controller.js
@@ -1,83 +1,63 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
-export default class extends Controller{
-    static values = {
-        condition: String,
-        hiddenClass : {type: String, default: "d-none"}
-    }
+export default class extends Controller {
+  static values = {
+    hiddenClass: { type: String, default: 'd-none' }
+  }
 
-    static targets = ["hideButton", "showButton", 'item' ]
+  static targets = ['hideButton', 'showButton', 'item']
 
-    connect(){
-        let element
-        if (
-            this.element.childNodes[0] &&
-            this.element.childNodes[0].childNodes[0] &&
-            this.element.childNodes[0].childNodes[0].childNodes[3]
-          ){
-            element = this.element.childNodes[0].childNodes[0].childNodes[3]
-            if(element.tagName == "SELECT"){
-                document.getElementById(element.value).classList.remove(this.hiddenClassValue)
-            }
-        }
-    }
-    toggle(event) {
-        if (!this.conditionValue) {
-            this.#toggle(event)
-        } else if (this.#shown() && !this.#conditionChecked(event)) {
-            this.#toggle(event)
-        } else if (!this.#shown() && this.#conditionChecked(event)) {
-            this.#toggle(event)
-        }
-    }
+  /*
+      Toggle all the items
+   */
+  toggle (event) {
+    this.#getItems(event).forEach((s) => {
+      s.classList.toggle(this.hiddenClassValue)
+    })
+  }
 
-    select(event){
-        console.log('woqsfqsdfqsdfrking')
-        let myItems = document.getElementsByClassName(event.target.parentNode.parentNode.getAttribute('data-items'))
-        for(var i = 0; i < myItems.length; i++) {
-            myItems[i].classList.add('d-none');
-        }
-        document.getElementById(event.target.value).classList.remove(this.hiddenClassValue)
-    }
+  /*
+      Hide all the items except the selected one
+   */
+  select (event) {
+    let selectedValue = event.target.value
+    let items = this.#getItems(event)
+    items.forEach((s) => {
+      s.classList.add(this.hiddenClassValue)
+    })
 
-    show(event){
-        this.#getItems(event).classList.remove(this.hiddenClassValue)
-        this.hideButtonTarget.classList.remove(this.hiddenClassValue)
-        this.showButtonTarget.classList.add(this.hiddenClassValue)
-    }
-    hide(event){
-        this.#getItems(event).classList.add(this.hiddenClassValue)
-        this.hideButtonTarget.classList.add(this.hiddenClassValue)
-        this.showButtonTarget.classList.remove(this.hiddenClassValue)
-    }
-    
+    items.forEach((s) => {
+      if (selectedValue === s.dataset.value) {
+        s.classList.remove(this.hiddenClassValue)
+      }
+    })
+  }
 
-    #conditionChecked(event) {
-        return this.conditionValue === event.target.value
-    }
+  show (event) {
+    this.#getItems(event).forEach((s) => s.classList.remove(this.hiddenClassValue))
+    this.hideButtonTarget.classList.remove(this.hiddenClassValue)
+    this.showButtonTarget.classList.add(this.hiddenClassValue)
+  }
 
-    #shown() {
-        return !this.itemTargets[0].classList.contains(this.class);
-    }
+  hide (event) {
+    this.#getItems(event).forEach((s) => s.classList.add(this.hiddenClassValue))
+    this.hideButtonTarget.classList.add(this.hiddenClassValue)
+    this.showButtonTarget.classList.remove(this.hiddenClassValue)
+  }
 
-    #toggle(event) {        
-        this.#getItems(event).forEach((s) => {
-          s.classList.toggle(this.hiddenClassValue);
-        });
-    }
+  #ItemById (event) {
+    let button = event.target.closest('[data-id]')
+    return document.getElementById(button.dataset.id)
+  }
 
-    #ItemById(event){
-        let button = event.target.closest("[data-id]");
-        return document.getElementById(button.dataset.id);
+  #getItems (event) {
+    let items
+    if (this.hasItemTarget) {
+      items = this.itemTargets
+    } else {
+      items = [this.#ItemById(event)]
     }
-    #getItems(event){
-        let items
-        if(this.hasItemTarget){
-            items = this.itemTarget
-        } else {
-            items = [this.#ItemById(event)]
-        }
-        return items
-    }
+    return items
+  }
 
 }
