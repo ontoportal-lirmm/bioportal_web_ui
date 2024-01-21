a2f16d0f6403826cabc0eb006426150c2d9e8469
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2024-01-21 04:53:54 +0100

    add copy to clipboard component

---
+++ app/components/clipboard_component/clipboard_component_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+
+  static targets = ['content', 'copy', 'check']
+  static values = {
+    hiddenCss: { type: String, default: 'd-none' },
+    successDuration: { type: Number, default: 2000 }
+  }
+
+  connect () {
+    console.log('Hello, Stimulus!', this.element)
+  }
+
+  copy () {
+    const text = this.contentTarget.innerHTML || this.contentTarget.value
+    navigator.clipboard.writeText(text).then(() => {
+      this.#copied()
+    })
+  }
+
+  #copied () {
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+
+    this.#toggleCopy()
+
+    this.timeout = setTimeout(() => {
+      this.#toggleCopy()
+    }, this.successDurationValue)
+  }
+
+  #toggleCopy () {
+    this.copyTarget.classList.toggle(this.hiddenCssValue)
+    this.checkTarget.classList.toggle(this.hiddenCssValue)
+  }
+}
