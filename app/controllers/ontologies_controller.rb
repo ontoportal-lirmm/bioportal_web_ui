734e9ed736f83d2164218d6211ab69ddeffc6c67
Bilel KIHAL <kb_kihal@esi.dz>
2024-02-14 11:30:18 +0100

    make filter by categories work in ontologies selector

--- app/controllers/ontologies_controller.rb
+++ app/controllers/ontologies_controller.rb
@@ -448,12 +448,18 @@ class OntologiesController < ApplicationController
     @ontologies = LinkedData::Client::Models::Ontology.all
     @total_ontologies_number = @ontologies.length
     @input = params[:input] || ''
-    #Filter by input according to ontology's name + ontology's acronym
+
     @ontologies = @ontologies.select { |ontology| ontology.name.downcase.include?(@input.downcase) || ontology.acronym.downcase.include?(@input.downcase)}
-    #Filter by groups
+
     if params[:groups] 
       @ontologies = @ontologies.select do |ontology|
         (ontology.group & params[:groups]).any?
+      end
+    end
+
+    if params[:categories]
+      @ontologies = @ontologies.select do |ontology|
+        (ontology.hasDomain & params[:categories]).any?
       end
     end
     
