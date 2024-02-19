79b2c11dafa160d387ce1b4f9d9a78d14e4c3e07
Bilel KIHAL <kb_kihal@esi.dz>
2024-02-19 13:11:34 +0100

    make select all work in ontologies selector

--- app/javascript/controllers/ontologies_selector_controller.js
+++ app/javascript/controllers/ontologies_selector_controller.js
@@ -1,11 +1,16 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ['submit']
+    static targets = ['submit', 'ontology']
     input(){
         this.submitTarget.click()
     }
     change(){
         this.submitTarget.click()
+    }
+    selectall(){
+        for (var i = 0; i < this.ontologyTargets.length; i++) {
+            this.ontologyTargets[i].querySelector('input').checked = true
+        }
     }
 }
