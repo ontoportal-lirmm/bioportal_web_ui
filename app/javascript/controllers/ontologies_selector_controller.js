b30c5517f5d3e54f37e46bb70bf03e32d47ce453
Bilel KIHAL <kb_kihal@esi.dz>
2024-02-19 17:25:20 +0100

    show tab selected checks count in ontologies selector

--- app/javascript/controllers/ontologies_selector_controller.js
+++ app/javascript/controllers/ontologies_selector_controller.js
@@ -1,12 +1,13 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ['submit', 'ontology']
+    static targets = ['submit', 'ontology', 'table']
     input(){
         this.submitTarget.click()
     }
-    change(){
+    change(event){
         this.submitTarget.click()
+        this.#updateTableNumbers(event)
     }
     selectall(event){
         if (event.target.innerHTML == '\nselect all\n'){
@@ -22,4 +23,32 @@ export default class extends Controller {
         }
         
     }
+    #updateTableNumbers(){
+        let navItems = this.tableTarget.querySelectorAll('.nav-item')
+        for (var i = 0; i < navItems.length; i++) {
+            this.#updateNavItemCount(navItems[i])
+        }        
+    }
+    #updateNavItemCount(navItem){
+        let tabPane = this.tableTarget.querySelector('.tab-pane'+navItem.getAttribute('data-target'))
+        let inputs = tabPane.querySelectorAll('input')
+        let count = 0
+        for (var i = 0; i < inputs.length; i++) {
+            if (inputs[i].checked){
+                count++;
+            }
+        } 
+        let itemTitle = navItem.querySelector('a').innerHTML 
+        let regex = /\(\d+\)/;
+        if (itemTitle.endsWith(")")){
+            
+            navItem.querySelector('a').innerHTML = itemTitle.replace(regex, '(' + count + ')');
+        } else {
+            navItem.querySelector('a').innerHTML = `${itemTitle} (${count})`;
+        }
+
+        if (count==0){
+            navItem.querySelector('a').innerHTML = itemTitle.replace(regex, '').trim();
+        }
+    }    
 }
