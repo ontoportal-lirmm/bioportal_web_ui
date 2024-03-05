80672f75dee851994096d16446afee8fce7194ff
Sirine Mhedhbi <sirine18mhedhbi@gmail.com>
2024-03-05 16:29:50 +0100

    add the logic to replace ontology with semantic term

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -642,4 +642,31 @@ module ApplicationHelper
   end
 
 
+  def t(*args)
+    translation = I18n.t(*args).downcase
+    term =  "ontology"
+    plural_term = "ontologies"
+    single_term = "an ontology"
+    resource = I18n.t("resource_term.#{$RESOURCE_TERM}")
+    resources = I18n.t("resource_term.#{$RESOURCE_TERM}_plural")
+    a_resource = I18n.t("resource_term.#{$RESOURCE_TERM}_single")
+
+    if translation.include?(term)
+      replacement = resource.capitalize
+      replacement = resource if translation.include?(term)
+      if translation.include?(single_term)
+        term = single_term
+        replacement = a_resource
+      end
+      translation.gsub(term, replacement)
+
+    elsif translation.include?(plural_term)
+      replacement = resources.capitalize
+      replacement = resources if translation.include?(plural_term)
+      translation.gsub(plural_term, replacement)
+    else
+      I18n.t(*args)
+    end
+  end
+
 end
