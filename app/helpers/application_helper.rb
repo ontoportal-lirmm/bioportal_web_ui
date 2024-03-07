15d7fc44a225724e4c743add6934a2e64ef92367
Sirine Mhedhbi <sirine18mhedhbi@gmail.com>
2024-03-07 12:58:01 +0100

    Remove the logic of configuring the ontology from the ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -641,32 +641,4 @@ module ApplicationHelper
     list&.join(',') || ''
   end
 
-
-  def t(*args)
-    translation = I18n.t(*args).downcase
-    term = I18n.t("resource_term.ontology")
-    plural_term = I18n.t("resource_term.ontology_plural")
-    single_term = I18n.t("resource_term.ontology_single")
-    resource = I18n.t("resource_term.#{$RESOURCE_TERM}")
-    resources = I18n.t("resource_term.#{$RESOURCE_TERM}_plural")
-    a_resource = I18n.t("resource_term.#{$RESOURCE_TERM}_single")
-
-    if translation.include?(term)
-      replacement = resource.capitalize
-      replacement = resource if translation.include?(term)
-      if translation.include?(single_term)
-        term = single_term
-        replacement = a_resource
-      end
-      translation.gsub(term, replacement)
-
-    elsif translation.include?(plural_term)
-      replacement = resources.capitalize
-      replacement = resources if translation.include?(plural_term)
-      translation.gsub(plural_term, replacement)
-    else
-      I18n.t(*args)
-    end
-  end
-
 end
