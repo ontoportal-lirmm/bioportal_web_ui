5b9462bf4f27454f6187a90ddd8bdaa629215018
Bilel KIHAL <kb_kihal@esi.dz>
2024-03-15 12:20:02 +0100

    clean form url controller code

--- app/javascript/controllers/form_url_controller.js
+++ app/javascript/controllers/form_url_controller.js
@@ -2,40 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 
 export default class extends Controller {
-    static targets = ['form']
-    static values = {
-        options: Array
-    }
-    
+ 
     submit(event){
         event.preventDefault();
-        let selectedOptions, hiddenInput, optionString
-        for(let i=0; i<this.optionsValue.length; i++){
-            selectedOptions = event.currentTarget.querySelector(`select[name="${this.optionsValue[i]}[]"]`).selectedOptions;
-            if(selectedOptions.length>0){
-                optionString = this.#array_to_string(selectedOptions)
-                hiddenInput = this.#create_hidden_input_element(optionString, this.optionsValue[i])
+        let selectedOptions, hiddenInput, optionString, selectElem
+        let  allSelects = this.element.querySelectorAll('select[name$="[]"]');
+        for (const select of allSelects) {
+            const selectElem = select.name.substring(0, select.name.indexOf('['));
+            const selectedOptions = select.selectedOptions;
+            if (selectedOptions.length > 0) {
+                const optionString = Array.from(selectedOptions, option => option.value).join(",");
+                const hiddenInput = this.#create_hidden_input_element(optionString, selectElem);
                 event.currentTarget.appendChild(hiddenInput);
-                this.#remove_html_element(event.currentTarget.querySelector(`select[name="${this.optionsValue[i]}[]"]`))
+                select.remove();
             }
         }
+        
         event.currentTarget.submit();
     }
-    #array_to_string(selectedOptions){
-        let optionString = "";
-        for (const option of selectedOptions) {
-            optionString += option.value + ",";
-        }
-        return optionString.slice(0, -1);
-    }
+
     #create_hidden_input_element(optionString, name){
         let hiddenInput = document.createElement('input');
         hiddenInput.type = 'hidden';
         hiddenInput.name = name;
         hiddenInput.value = optionString;
         return hiddenInput
-    }
-    #remove_html_element(element){
-        return element.remove();
     }
 }
