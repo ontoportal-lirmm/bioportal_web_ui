15c059455d5639887ebc06c282fbbdf15b3acf93
Bilel KIHAL <kb_kihal@esi.dz>
2024-03-19 20:35:47 +0100

    add another type to the loader component that uses the style of the three dots

--- app/components/loader_component.rb
+++ app/components/loader_component.rb
@@ -3,23 +3,36 @@
 class LoaderComponent < ViewComponent::Base
   include ActionView::Helpers::TagHelper
 
-  def initialize(small: false)
+  def initialize(small: false, type: nil)
     super
     @small = small
+    @type = type
   end
 
   def small_class
     @small ? 'spinner-border-sm' : ''
   end
 
+  def type
+    !@type.eql?('2')
+  end
+
   def call
-    content_tag(:div, class: 'd-flex align-items-center flex-column') do
-      content_tag(:div, class: "spinner-border #{small_class}") do
-        content_tag(:span) do
-          t('components.loading')
+    if type
+      content_tag(:div, class: 'd-flex align-items-center flex-column') do
+        content_tag(:div, class: "spinner-border #{small_class}") do
+          content_tag(:span) do
+            t('components.loading')
+          end
+          content_tag(:div, class: 'spinner-text my-2') do
+            t('components.loading')
+          end
         end
-        content_tag(:div, class: 'spinner-text my-2') do
-          t('components.loading')
+      end
+    else
+      content_tag(:div, class: 'loader-component') do
+        content_tag(:div, class: "lds-ellipsis loader-component") do
+          4.times.map{content_tag(:div)}.join.html_safe
         end
       end
     end
