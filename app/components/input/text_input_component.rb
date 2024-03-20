851bf8fcddc47db9754b165276af751fe68f03bf
Bilel KIHAL <kb_kihal@esi.dz>
2024-03-20 10:51:40 +0100

    replace ontologies selector text input by text input helper

--- app/components/input/text_input_component.rb
+++ app/components/input/text_input_component.rb
@@ -2,12 +2,12 @@
 
 class Input::TextInputComponent < Input::InputFieldComponent
   def initialize(label: '', name:, value: nil, placeholder: '', error_message: '', helper_text: '', disabled: false, id: '', data: {})
-    super(label: label, name: name, value: value,  placeholder: placeholder, error_message: error_message, helper_text: helper_text, disabled: disabled, id: id)
+    super(label: label, name: name, value: value,  placeholder: placeholder, error_message: error_message, helper_text: helper_text, disabled: disabled, id: id, data: data)
   end
 
   def call
     render Input::InputFieldComponent.new(label: @label, name: @name, value: @value,  placeholder: @placeholder,
                                           error_message: @error_message, helper_text: @helper_text,
-                                          type: @type, disabled: @disabled)
+                                          type: @type, disabled: @disabled, data: @data)
   end
 end
