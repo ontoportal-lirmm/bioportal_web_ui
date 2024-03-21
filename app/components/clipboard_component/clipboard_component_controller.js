1836108da7f8517b0ae09af4a97170d41cbe91bb
imadbourouche <ji_bourouche@esi.dz>
2024-03-21 21:31:46 +0100

    Add the copy functionality to the resource raw data
    for formts: json, xml, ntriples and turtle

--- app/components/clipboard_component/clipboard_component_controller.js
+++ app/components/clipboard_component/clipboard_component_controller.js
@@ -14,6 +14,21 @@ export default class extends Controller {
       this.#copied()
     })
   }
+  
+  copy_concept_content () {
+    const activeTab = document.querySelector('.concepts-content-format .tab-content .tab-pane.active');
+    if(activeTab.id != 'html_content'){
+      let resource_content = activeTab.querySelector('.content-finder-result').getAttribute('data-content-finder-result-value')
+      navigator.clipboard.writeText(resource_content).then(() => {
+          this.#copied();
+      })
+    }else{
+      navigator.clipboard.writeText("").then(() => {
+        this.#copied()
+      })
+    }
+    
+  }
 
   #copied () {
     if (this.timeout) {
