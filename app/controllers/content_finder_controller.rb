96baa5a27e1993b839732653fda991792a84ad54
imadbourouche <ji_bourouche@esi.dz>
2024-03-27 13:43:16 +0100

    Change the controller to add use the accept headers
    - use the Net::HTTP instead of Linked::Client::HTTP to use the accept header

--- app/controllers/content_finder_controller.rb
+++ app/controllers/content_finder_controller.rb
@@ -5,18 +5,28 @@ class ContentFinderController < ApplicationController
     def index
         if params[:acronym] && params[:uri]
             @acronym = params[:acronym]
-            ontology = LinkedData::Client::Models::Ontology.find_by_acronym(params[:acronym]).first
-            sub = ontology.explore.latest_submission({ include: 'submissionId' })
-            params[:output_format] = params[:output_format].presence || 'json'
-            params[:acronym] = sub.id.gsub(REST_URI, 'http://data.bioontology.org/')
-            if params[:output_format] == 'html'
-                params[:output_format] = 'json'
-            end
             @format = params[:output_format]
-            if params[:output_format] == 'json'
-                @result = LinkedData::Client::HTTP.post("/dereference_resource", params).to_h.to_json
-            else
-                @result = LinkedData::Client::HTTP.post("/dereference_resource", params)
+
+            url = URI.parse("#{REST_URI}ontologies/#{params[:acronym].strip}/resolve/#{CGI.escape(params[:uri].strip)}")
+            http = Net::HTTP.new(url.host, url.port)
+            http.use_ssl = true if url.scheme == 'https'
+            request = Net::HTTP::Get.new(url)
+            request.body = "apikey=#{API_KEY}"
+    
+            case params[:output_format]
+            when 'json'
+              request['Accept'] = "application/json"
+            when 'xml'
+              request['Accept'] = "application/xml"
+            when 'ntriples'
+              request['Accept'] = "application/n-triples"
+            when 'turtle'
+              request['Accept'] = "text/turtle"
+            end
+            response = http.request(request)
+            @result = ""
+            if response.code == '200'
+              @result = response.body.force_encoding(Encoding::UTF_8)
             end
         end
         render 'content_finder/index'
