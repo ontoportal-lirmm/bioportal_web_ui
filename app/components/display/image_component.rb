98b14c0f2807ad2fde990d90a24369366349a4d8
Bilel KIHAL <kb_kihal@esi.dz>
2024-03-28 00:51:38 +0100

    fix summary page zoom icon

--- app/components/display/image_component.rb
+++ app/components/display/image_component.rb
@@ -18,7 +18,7 @@ class Display::ImageComponent < ViewComponent::Base
 
   def depiction_with_modal(depiction_url)
     img_tag = image_tag(depiction_url, class: 'image-content')
-    loop_icon_tag = content_tag(:span , image_tag('icons/loop.svg'), class: 'loop_icon')
+    loop_icon_tag = content_tag(:span , inline_svg_tag('icons/loop.svg'), class: 'loop_icon')
     modal_url = "/ajax/images/show?url=#{depiction_url}"
     modal_options = { data: { show_modal_title_value: @title, show_modal_size_value: 'modal-xl' } }
 
