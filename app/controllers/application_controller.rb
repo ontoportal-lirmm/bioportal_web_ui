e99d29b8dfbe83318cf939f175241c21ae1671c7
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2024-03-27 01:36:32 +0100

    fix duplicated translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,11 @@ class ApplicationController < ActionController::Base
   include InternationalisationHelper
 
   before_action :set_locale
-  
+
+  def self.t(*args)
+    InternationalisationHelper.t(*args)
+  end
+
   # Sets the locale based on the locale cookie or the value returned by detect_locale.
   def set_locale    
     I18n.locale = cookies[:locale] || detect_locale
