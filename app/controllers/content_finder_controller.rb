0e57aaa8dff069c22efd2d1362c313aa5cec1469
Imad Bourouche <ji_bourouche@esi.dz>
2024-04-07 11:55:32 +0200

    Feature: Adding Content Finder Page (#511)
    * Adding Content Finder Page
    
    * use ontologies selector instead of the url input
    
    * Change the controller to add use the accept headers
    
    - use the Net::HTTP instead of Linked::Client::HTTP to use the accept header
    
    * use ontologies selector in the content finder page
    
    * use the rest_url helper instead of the global variable REST_URI
    
    * remove content finder link from the topnav
    
    * close the array items in navitems method that causes an error
    
    * use Faraday instead of Net::HTTP
    
    * Remove result in content_finder_controller and use this.element.textContent
    
    * use this.contentTarget.textContent instead element in the content finder
    
    * Add html view using the concept details component
    
    * use tool layout for the content finder page
    
    ---------
    
    Co-authored-by: Syphax Bouazzouni <gs_bouazzouni@esi.dz>

---
+++ app/controllers/content_finder_controller.rb
@@ -0,0 +1,34 @@
+require 'faraday'
+class ContentFinderController < ApplicationController
+
+    def index
+        if params[:acronym] && params[:uri]
+            @acronym = params[:acronym]
+            @format = params[:output_format]
+            case params[:output_format]
+            when 'json'
+              accept_header = "application/json"
+            when 'xml'
+              accept_header = "application/xml"
+            when 'ntriples'
+              accept_header = "application/n-triples"
+            when 'turtle'
+              accept_header = "text/turtle"
+            end
+
+            url = URI.parse("#{rest_url}/ontologies/#{params[:acronym].strip}/resolve/#{helpers.escape(params[:uri].strip)}")
+            conn = Faraday.new(url: url) do |faraday|
+              faraday.headers['Accept'] = accept_header
+              faraday.adapter Faraday.default_adapter
+              faraday.headers['Authorization'] = "apikey token=#{API_KEY}"
+            end
+            
+            response = conn.get
+            @result=""
+            if response.success?
+                @result = response.body.force_encoding(Encoding::UTF_8)
+            end
+        end
+        render 'content_finder/index', layout: 'tool'
+    end
+end
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -102,3 +102,6 @@ application.register('form-url', FormUrlController)
 
 import OntologiesSelector from "./ontologies_selector_controller"
 application.register("ontologies-selector", OntologiesSelector)
+
+import ContentFinderController from "./content_finder_controller"
+application.register("content-finder", ContentFinderController)
