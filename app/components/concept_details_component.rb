92484d734d8081cc44646a43543fc4fbdab8c454
Bilel KIHAL <kb_kihal@esi.dz>
2024-04-23 15:59:19 +0200

    clean concept details component code

--- app/components/concept_details_component.rb
+++ app/components/concept_details_component.rb
@@ -73,7 +73,7 @@ class ConceptDetailsComponent < ViewComponent::Base
     all_keys = concept_properties&.keys || []
     top_set = properties_set_by_keys(top_keys, concept_properties, exclude_keys)
     bottom_set = properties_set_by_keys(bottom_keys, concept_properties, exclude_keys)
-    leftover = properties_set_by_keys(all_keys - top_keys - bottom_keys, concept_properties, exclude_keys)
+    leftover = properties_set_by_keys(all_keys, concept_properties, exclude_keys)
     leftover = leftover.reject { |key, _| top_set.key?(key) }
     [top_set, leftover, bottom_set]
   end
