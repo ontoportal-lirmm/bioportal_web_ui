5681984f956191cc3db3f8fc94e4dc75828a3ed1
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2024-05-10 17:28:57 +0200

    remove binding.pry from final federation code

--- config/initializers/ontologies_api_client.rb
+++ config/initializers/ontologies_api_client.rb
@@ -7,5 +7,5 @@ LinkedData::Client.config do |config|
   config.debug_client = $DEBUG_RUBY_CLIENT || false
   config.debug_client_keys = $DEBUG_RUBY_CLIENT_KEYS || []
   config.apikey = $API_KEY
-  config.federated_portals = $FEDERATED_PORTALS || []
+  config.federated_portals = $FEDERATED_PORTALS || {}
 end
