37534e15b84e17d5a35b7876a166b6a4cabc0de4
Bilel KIHAL <kb_kihal@esi.dz>
2024-05-23 08:49:26 +0200

    check if all the inputs and filters are present in annotator page test

--- test/system/annotator_page_test.rb
+++ test/system/annotator_page_test.rb
@@ -7,16 +7,32 @@ class AnnotatorPageTest < ApplicationSystemTestCase
         @apikey = LinkedData::Client.settings.apikey
         @host = LinkedData::Client.settings.rest_url
         @annotator_api = "https://services.stageportal.lirmm.fr/annotator"
-        @annotator_text_area = "div#annotator_text_area"
+        @annotator_text_area = ".annotator-page-text-area > textarea"
     end
 
     test "go to annotator page and check if all the inputs and filters are there" do
         visit root_url
         click_link(href: '/annotator')
         assert_selector @annotator_text_area
+        assert_selector 'div.insert-sample-text-button'
+        assert_selector 'label[for="chips-whole_word_only-check"]'
+        assert_selector 'label[for="chips-longest_only-check"]'
+        assert_selector 'label[for="chips-expand_mappings-check"]'
+        assert_selector 'label[for="chips-exclude_numbers-check"]'
+        assert_selector 'label[for="chips-exclude_synonyms-check"]'
+        assert_selector 'div.select-ontologies'
+        find('div.advanced-options-button').click
+        assert_selector '#umls_semantic_types_selector'
+        assert_selector '#umls_semantic_groups_selector'
+        assert_selector '#include_accentors'
+        assert_selector '#include_score'
+        assert_selector 'input[name="score_threshold"]'
+        assert_selector 'input[name="confidence_threshold"]'
+        assert_selector 'label[for="chips-fast_context-check"]'
+        assert_selector 'label[for="chips-lemmatize-check"]'
     end
 
-    test "go to annotator page insert sample text and get annotations"
+    test "go to annotator page insert sample text and get annotations" do
         visit root_url
         click_link(href: '/annotator')
         find(@annotator_text_area).fill_in(with: 'Melanoma is a malignant tumor of melanocytes found mainly in the skin but also in the intestine and the eye.')
