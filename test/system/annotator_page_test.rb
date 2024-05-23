05e2645e17a0bead857810e04e4b38f276a01f8c
Bilel KIHAL <kb_kihal@esi.dz>
2024-05-23 21:25:06 +0200

    add comments in annotator page tests

--- test/system/annotator_page_test.rb
+++ test/system/annotator_page_test.rb
@@ -19,13 +19,18 @@ class AnnotatorPageTest < ApplicationSystemTestCase
         click_link(href: '/annotator')
         assert_selector @annotator_text_area
         assert_selector 'div.insert-sample-text-button'
+        # Check if there are the annotator's options
         assert_selector 'label[for="chips-whole_word_only-check"]'
         assert_selector 'label[for="chips-longest_only-check"]'
         assert_selector 'label[for="chips-expand_mappings-check"]'
         assert_selector 'label[for="chips-exclude_numbers-check"]'
         assert_selector 'label[for="chips-exclude_synonyms-check"]'
         assert_selector 'div.select-ontologies'
+
+        # Open the advanced options
         find('div.advanced-options-button').click
+
+        # Check if there are the advanced options
         assert_selector '#umls_semantic_types_selector'
         assert_selector '#umls_semantic_groups_selector'
         assert_selector '#include_accentors'
@@ -39,7 +44,11 @@ class AnnotatorPageTest < ApplicationSystemTestCase
     test "go to annotator page insert sample text and get annotations" do
         visit root_url
         click_link(href: '/annotator')
+
+        # Fill the annotator's text area input by a sample text
         find(@annotator_text_area).fill_in(with: 'Melanoma is a malignant tumor of melanocytes found mainly')
+
+        # Mock the api call for the annotator with the entered text
         WebMock.enable!
         stub_request(:get, "https://services.stageportal.lirmm.fr/annotator?class_hierarchy_max_level=None&confidence_threshold=0&score_threshold=0&text=Melanoma%20is%20a%20malignant%20tumor%20of%20melanocytes%20found%20mainly&whole_word_only=true")
             .with(
@@ -54,16 +63,25 @@ class AnnotatorPageTest < ApplicationSystemTestCase
         
         find(".annotator-page-button #annotator").click
 
+        # Check if we get the table of annotations
         assert_selector 'table#annotator-table'
+
+        # Check if the number of annotations is 4
         assert_equal 5, page.all('tr').count
+
+        # Check if the action buttons below the table are there (json, rdf, cite us and api doc buttons)
         assert_selector '#annotator_json'
         assert_selector '#annotator_rdf'
         assert_selector '#annotator_cite_us'
         assert_selector '#annotator_api_doc'
         
+        # Clear the sample text in the annotator text area
         find(@annotator_text_area).native.clear
+
+        # Fill it with a text that will return an empty state
         find(@annotator_text_area).fill_in(with: 'mainly')
 
+        # Mock the api call of the annotator to get and return an empty result
         stub_request(:get, "https://services.stageportal.lirmm.fr/annotator?class_hierarchy_max_level=None&confidence_threshold=0&score_threshold=0&text=mainly&whole_word_only=true")
             .with(
                 headers: {
@@ -75,7 +93,8 @@ class AnnotatorPageTest < ApplicationSystemTestCase
             .to_return(status: 200, body: ([]).to_json, headers: {})
 
         find(".annotator-page-button #annotator").click
-
+        
+        # Check if we got the empty state correctly
         assert_selector 'div.browse-empty-illustration'
         
         WebMock.disable!
