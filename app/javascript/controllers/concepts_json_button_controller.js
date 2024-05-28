c6dfd561e05d83b0625a3980d51a5e6ef9aa4e45
Bilel KIHAL <kb_kihal@esi.dz>
2024-05-28 11:33:54 +0200

    clean concepts json button controller code

--- app/javascript/controllers/concepts_json_button_controller.js
+++ app/javascript/controllers/concepts_json_button_controller.js
@@ -5,23 +5,23 @@ export default class extends Controller {
         classUrl: String,
         apiKey: String
       }
+    static targets = ['button']
 
-    update(){
+    update(event){
         const baseClassUrl = this.classUrlValue;
         const apikey = `apikey=${this.apiKeyValue}`;
-        const tabsList = document.querySelectorAll('#concept_tabs_container .nav-item');
+        const tabsList = event.currentTarget.querySelectorAll('.nav-item');
         const links = {
             details_tab: `${baseClassUrl}?${apikey}&display=all`,
             instances_tab: `${baseClassUrl}/instances?${apikey}`,
             notes_tab: `${baseClassUrl}/notes?${apikey}`,
             mappings_tab: `${baseClassUrl}/mappings?${apikey}`,
-            visualization_tab: ''
         };
 
         const selectedItemId = Array.from(tabsList).find(tab => tab.classList.contains('active'))?.querySelector('a').id; 
 
         const jsonLink = links[selectedItemId];
-        const conceptsJsonLink = document.querySelector('#concepts_json_link a');
+        const conceptsJsonLink = this.buttonTarget.querySelector('a');
         
         if (event.target.id === 'visualization_tab') {
         conceptsJsonLink.style.display = 'none';
