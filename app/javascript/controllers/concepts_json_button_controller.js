6c3f4d2ccc361807da950e5f454b6d2edaf88ce6
Bilel KIHAL <kb_kihal@esi.dz>
2024-05-28 15:24:36 +0200

    clean concepts json button related code

--- app/javascript/controllers/concepts_json_button_controller.js
+++ app/javascript/controllers/concepts_json_button_controller.js
@@ -1,34 +1,19 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static values = {
-        classUrl: String,
-        apiKey: String
-      }
     static targets = ['button']
 
-    update(event){
-        const baseClassUrl = this.classUrlValue;
-        const apikey = `apikey=${this.apiKeyValue}`;
-        const tabsList = event.currentTarget.querySelectorAll('.nav-item');
-        const links = {
-            details_tab: `${baseClassUrl}?${apikey}&display=all`,
-            instances_tab: `${baseClassUrl}/instances?${apikey}`,
-            notes_tab: `${baseClassUrl}/notes?${apikey}`,
-            mappings_tab: `${baseClassUrl}/mappings?${apikey}`,
-        };
-
-        const selectedItemId = Array.from(tabsList).find(tab => tab.classList.contains('active'))?.querySelector('a').id; 
-
-        const jsonLink = links[selectedItemId];
-        const conceptsJsonLink = this.buttonTarget.querySelector('a');
-        
-        if (event.target.id === 'visualization_tab') {
-        conceptsJsonLink.style.display = 'none';
-        } else {
-        conceptsJsonLink.style.display = 'flex';
-        conceptsJsonLink.href = jsonLink;
-        }
+    update(){
+        setTimeout(() => {
+            const tabsList = this.element.querySelectorAll('.nav-item');
+            const jsonLink = Array.from(tabsList).find(tab => tab.classList.contains('active'))?.querySelector('a').getAttribute('data-json-link');
+            const conceptsJsonLink = this.buttonTarget.querySelector('a');
+            if (jsonLink) {
+                conceptsJsonLink.href = jsonLink;
+                conceptsJsonLink.style.display = 'flex'
+            } else {
+                conceptsJsonLink.style.display = 'none'
+            }
+        }, 1);
     }
-
 }
