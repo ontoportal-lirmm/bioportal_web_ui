db95b8f4ad813d4b77b2afe23f3e44cc177a28f3
Bilel KIHAL <kb_kihal@esi.dz>
2024-07-02 18:11:28 +0200

    remove invalidate cache for the user calls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -208,7 +208,7 @@ module ApplicationHelper
   end
 
   def subscribed_to_ontology?(ontology_acronym, user)
-    user = LinkedData::Client::Models::User.find(user.username, {include: 'subscription', invalidate_cache: true}) if user.subscription.nil?
+    user = LinkedData::Client::Models::User.find(user.username, {include: 'subscription'}) if user.subscription.nil?
     return false if user.subscription.nil? or user.subscription.empty?
     user.subscription.each do |sub|
       #sub = {ontology: ontology_acronym, notification_type: "NOTES"}
