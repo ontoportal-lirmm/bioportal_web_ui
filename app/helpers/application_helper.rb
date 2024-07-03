6d8740ac79ca9b99a87cff1ffe19b7e58bdf4795
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2024-07-03 05:55:44 +0200

    feature: Add cookie banner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -648,6 +648,10 @@ module ApplicationHelper
     end
   end
 
+  def link_button_component(href: , value: , id:, size: nil)
+    render Buttons::RegularButtonComponent.new(id:id, value: value, variant: "primary", type: 'link', href: href, size: size)
+  end
+
   def save_button_component(class_name: nil, id: , value:, data: nil, size: nil, type: nil)
     content_tag(:div, data: data, class: class_name) do
       render Buttons::RegularButtonComponent.new(id:id, value: value, variant: "primary", state: 'regular', size: size, type: type) do |btn|
