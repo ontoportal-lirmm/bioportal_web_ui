2733132a8b0215dedb174625785824c4f32514a6
Muhammad <boukerfa.ma@gmail.com>
2024-09-11 23:08:14 +0100

    Properties layout harmonization

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -34,9 +34,7 @@ class PropertiesController < ApplicationController
     @acronym = params[:ontology]
     @property = get_property(params[:id],  @acronym, include: 'all')
 
-    redirect_to(ontology_path(id: params[:ontology], p: 'properties', propertyid: params[:id], lang: request_lang)) and return unless turbo_frame_request?
-
-    render partial: 'show'
+    redirect_to(ontology_path(id: params[:ontology], p: 'properties', propertyid: params[:id], lang: request_lang))
   end
 
 
