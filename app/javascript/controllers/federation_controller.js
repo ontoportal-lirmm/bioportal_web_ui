bf5fe76436249256ce0cdf65f0edf3f7f3cbdf69
Bilel KIHAL <kb_kihal@esi.dz>
2024-09-27 14:01:22 +0200

    disable input chips in federated search page for the portals that are not responding

--- app/javascript/controllers/federation_controller.js
+++ app/javascript/controllers/federation_controller.js
@@ -5,14 +5,40 @@ export default class extends Controller {
     static values = {
         config: Object,
       }
+    static targets = ['chips']
     connect() {
-        const style = document.createElement('style');
-        let styles = '';
-        Object.entries(this.configValue).forEach(([key, portal]) => {
-            styles += `.federated-icon-${key} path { fill: ${portal.color} !important; }\n`;
+      this.#initIconsStyle()
+      this.#initInputChips()
+    }
+    #initIconsStyle(){
+      const style = document.createElement('style');
+      let styles = '';
+      Object.entries(this.configValue).forEach(([key, portal]) => {
+          styles += `.federated-icon-${key} path { fill: ${portal.color} !important; }\n`;
+      });
+      style.innerHTML = styles;
+      document.head.appendChild(style);
+    }
+
+    #initInputChips(){
+      for (const key in this.configValue) {
+        $.ajax({
+          url: this.configValue[key].api,
+          type: 'GET',
+          success: function(response) {
+              console.log('working');
+          },
+          error: function() {
+              let chipsInputs = this.chipsTarget.querySelectorAll('input') 
+              Array.from(chipsInputs).forEach(input => {
+                if (input.value === this.configValue[key].name.toLowerCase()) {
+                    input.disabled = true; 
+                    input.parentNode.style.opacity = '0.5'
+                }
+              });
+          }.bind(this)
         });
-        style.innerHTML = styles;
-        document.head.appendChild(style);
+      }
     }
   }
   
