cd83029f8f9d8025bf8785f3c50353276eddbf37
Bilel KIHAL <kb_kihal@esi.dz>
2024-10-02 17:29:36 +0200

    use portal button helper in federated browse

--- app/helpers/federation_helper.rb
+++ app/helpers/federation_helper.rb
@@ -104,8 +104,8 @@ module FederationHelper
   end
 
   def portal_button(name: nil , color: nil , light_color: nil, link: nil, tooltip: nil)
-    content_tag(:a, href: link, target: '_blank', 'data-controller': 'tooltip', title: tooltip, class: 'button icon-right', style: color ? "background-color: #{light_color} !important" : '') do
-      inline_svg_tag('logos/ontoportal.svg', class: "federated-icon-#{name}") +
+    content_tag(:a, href: link, target: '_blank', 'data-controller': 'tooltip', title: tooltip, class: 'federation-portal-button button icon-right', style: color ? "background-color: #{light_color} !important" : '') do
+      inline_svg_tag('logos/ontoportal.svg', class: "federated-icon-#{name.downcase}") +
       content_tag(:div, class: 'text', style: color ? "color: #{color} !important" : '') do
         name.humanize.gsub("portal", "Portal")
       end
