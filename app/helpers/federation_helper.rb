58758a628942764d38bfde0fda724a0a44f68d64
Bilel KIHAL <kb_kihal@esi.dz>
2024-10-09 01:37:51 +0200

    use federation portal status in home page portals configuration

--- app/helpers/federation_helper.rb
+++ app/helpers/federation_helper.rb
@@ -123,7 +123,8 @@ module FederationHelper
 
   def federation_portal_status(portal_name: nil)
     Rails.cache.fetch("federation_portal_up_#{portal_name}", expires_in: 2.hours) do
-      portal_api = federated_portals[portal_name][:api]
+      portal_api = federated_portals[portal_name]&[:api]
+      return false unless portal_api
       portal_up = false
       begin
         response = Faraday.new(url: portal_api) do |f|
