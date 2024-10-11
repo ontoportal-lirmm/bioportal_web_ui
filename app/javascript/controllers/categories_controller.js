1f95275aca40fba8b6f1d8c43e7fe1fef832a829
Bilel KIHAL <kb_kihal@esi.dz>
2024-10-11 11:58:05 +0200

    fix check method in categories controller to select parents of a child when selected and deselect children of parent when deselected

--- app/javascript/controllers/categories_controller.js
+++ app/javascript/controllers/categories_controller.js
@@ -4,40 +4,55 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ['chips']
     static values = { categories: Array}
+
     check(event){
         const input = event.currentTarget.querySelector('input')
-        const parents = this.#nest_children_inside_parents()
+        const allInputs = this.chipsTarget.querySelectorAll('input')
+        const children = this.#categories_with_parents()
+        const parents = this.#categories_with_children(children)
+        
         if(input.value in parents){
-            const allInputs = this.chipsTarget.querySelectorAll('input')
-            const children = parents[input.value]
+            const parentChildren = parents[input.value]
             allInputs.forEach(i => {
-                if(children.includes(i.value)){
-                    if(input.checked){
-                        i.checked = true;
-                        i.dispatchEvent(new Event('change', { bubbles: true }));
-                    } else {
-                        i.checked = false;
-                        i.dispatchEvent(new Event('change', { bubbles: true }));
-                    }
-                    
+                if(parentChildren.includes(i.value) && !input.checked){
+                    i.checked = false;
+                    i.dispatchEvent(new Event('change', { bubbles: true })); 
+                }
+
+            });
+        }
+        if(input.value in children){
+            const childParents = children[input.value]
+            allInputs.forEach(i => {
+                if(childParents.includes(i.value) && input.checked){
+                    i.checked = true;
+                    i.dispatchEvent(new Event('change', { bubbles: true })); 
                 }
             });
         }
     }
-    #nest_children_inside_parents(){
-        const parents = this.categoriesValue.reduce((acc, category) => {
+
+    #categories_with_parents() {
+        const children = {};
+        this.categoriesValue.forEach(category => {
             if (category.parentCategory.length > 0) {
-                category.parentCategory.forEach(parent => {
-                    const parentAcronym = parent.split('/').pop();
-                    if (!acc[parentAcronym]) {
-                        acc[parentAcronym] = [];
-                    }
-                    acc[parentAcronym].push(category.acronym);
-                });
+                const parentsAcronyms = category.parentCategory.map(p => p.split('/').pop());
+                children[category.acronym] = parentsAcronyms;
             }
-            return acc;
-        }, {});
-        return parents
+        });
+        return children;
     }
 
+    #categories_with_children(children){
+        const parents = {};
+        Object.keys(children).forEach(child => {
+            children[child].forEach(parent => {
+                if (!parents[parent]) {
+                    parents[parent] = [];
+                }
+                parents[parent].push(child);
+            });
+        });
+        return parents
+    }
 }
