e2d9924160a6802e6f813999c8e4764bd2aed3f7
Syphax <gs_bouazzouni@esi.dz>
2024-10-22 04:58:08 +0200

    create dropdown component helper and add small argument to loader helper

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -1,6 +1,12 @@
 module ComponentsHelper
   include TermsReuses
 
+  def dropdown_component(id: ,title: nil, tooltip:nil , is_open: false, &block)
+    render DropdownContainerComponent.new(id: id, title: title, tooltip: tooltip, is_open: is_open) do |d|
+      capture(d, &block) if block_given?
+    end
+  end
+
   def portal_button(name: nil , color: nil , light_color: nil, link: nil, tooltip: nil)
     render FederatedPortalButtonComponent.new(name: name, color: color, link: link, tooltip: tooltip, light_color: light_color)
   end
@@ -183,8 +189,8 @@ module ComponentsHelper
     content_tag(:canvas, nil, data: data)
   end
 
-  def loader_component(type = 'pulsing')
-    render LoaderComponent.new(type: type)
+  def loader_component(type:'pulsing', small: false )
+    render LoaderComponent.new(type: type, small: small)
   end
 
   def info_tooltip(text, interactive: true)
