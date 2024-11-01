bcf103c7bd47e72b54e190244821b5f0e067e469
Syphax <gs_bouazzouni@esi.dz>
2024-11-01 18:08:39 +0100

    create label fetch component that for a id/uri fetch its label if found

---
+++ app/components/label_fetcher_component.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class LabelFetcherComponent < ViewComponent::Base
+  include UrlsHelper, Turbo::FramesHelper, ModalHelper
+
+  def initialize(id:, label: nil, link: nil, ajax_src: nil, open_in_modal: false)
+    super
+    @id = id
+    @link = link
+    @ajax_src = ajax_src
+    @open_in_modal = open_in_modal
+
+    if label.nil? || label.eql?(id)
+      @link = id
+      @target = '_blank'
+      @label = id
+    else
+      @label = label
+      @target = '_top'
+    end
+
+  end
+
+  def label_fetcher_container(&block)
+    id = "#{escape(@id)}_label"
+    if @ajax_src
+      render(TurboFrameComponent.new(id: id, src: @ajax_src, loading: 'lazy')) do |t|
+        t.loader do
+          render ChipButtonComponent.new(url: @id, text: "#{@id} #{render(LoaderComponent.new(small: true))}", type: 'clickable', target: '_blank')
+        end
+
+        t.error do
+          capture(&block)
+        end
+      end
+    else
+      turbo_frame_tag(id) do
+        capture(&block)
+      end
+    end
+
+  end
+end
