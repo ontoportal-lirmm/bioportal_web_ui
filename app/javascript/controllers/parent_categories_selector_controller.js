39a645db22bec20a07a157939e631a1f84cb8801
Bilel KIHAL <kb_kihal@esi.dz>
2025-01-02 16:07:40 +0100

    update the logic of associating parent categories with their children to select or deselect children with their parents

--- app/javascript/controllers/parent_categories_selector_controller.js
+++ app/javascript/controllers/parent_categories_selector_controller.js
@@ -14,19 +14,14 @@ export default class extends Controller {
         if(this.#id_to_acronym(input.value) in parents){
             const parentChildren = parents[this.#id_to_acronym(input.value)]
             allInputs.forEach(i => {
-                if(parentChildren.includes(this.#id_to_acronym(i.value)) && !input.checked){
-                    i.checked = false;
-                    i.dispatchEvent(new Event('change', { bubbles: true })); 
-                }
-
-            });
-        }
-        if(this.#id_to_acronym(input.value) in children){
-            const childParents = children[this.#id_to_acronym(input.value)]
-            allInputs.forEach(i => {
-                if(childParents.includes(this.#id_to_acronym(i.value)) && input.checked){
-                    i.checked = true;
-                    i.dispatchEvent(new Event('change', { bubbles: true })); 
+                if(parentChildren.includes(this.#id_to_acronym(i.value))){
+                    if(input.checked){
+                        i.checked = true;
+                        i.dispatchEvent(new Event('change', { bubbles: true })); 
+                    } else {
+                        i.checked = false;
+                        i.dispatchEvent(new Event('change', { bubbles: true })); 
+                    }
                 }
             });
         }
