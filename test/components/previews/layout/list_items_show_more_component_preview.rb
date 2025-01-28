58a933927b8ef1c4830528aa8c547b8f5eabf985
Bilel KIHAL <kb_kihal@esi.dz>
2025-01-28 07:43:58 +0100

    fix ListItemsShowMoreComponent was not displayed properly on lookbook

--- test/components/previews/layout/list_items_show_more_component_preview.rb
+++ test/components/previews/layout/list_items_show_more_component_preview.rb
@@ -1,14 +1,10 @@
 class Layout::ListItemsShowMoreComponentPreview < ViewComponent::Preview
 
   def default
-    render ListItemsShowMoreComponent.new do |component|
+    render ListItemsShowMoreComponent.new(max_items: 5) do |component|
       10.times do |i|
         component.container do
-          render ChipButtonComponent.new(
-            url: "#",
-            text: "Element #{i + 1}",
-            type: "clickable"
-          )
+          "Item #{i + 1}"
         end
       end
     end
