7597f377b99348cea6d61364902bf4565fa733c0
Imad Bourouche <ji_bourouche@esi.dz>
2025-03-06 22:26:56 +0100

    fix

--- test/integration/login_flows_test.rb
+++ test/integration/login_flows_test.rb
@@ -67,7 +67,7 @@ class LoginFlowsTest < ActionDispatch::IntegrationTest
     assert_select '.title', text: 'Username:'
     assert_select '.info', text: new_user.username
 
-    assert_select '.title', text: 'ORCID ID:'
+    assert_select '.title', text: 'ORCID:'
     assert_select '.info', text: new_user.orcidId
 
     assert_select '.title', text: 'GitHub ID:'
