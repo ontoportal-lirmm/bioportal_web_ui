27dbde181b385bb1a063949ff82c1d1047c2efc3
AmineBKF <boukerfa.ma@gmail.com>
2025-03-28 00:39:54 +0100

    fix naming conflict in js `ChartController`

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -95,7 +95,7 @@ import OntologiesSelector from "./ontologies_selector_controller"
 application.register("ontologies-selector", OntologiesSelector)
 
 import ChartController from "./agents_statistics_chart"
-application.register('mappings', ChartController)
+application.register('agents_statistics', ChartController)
 
 import MappingsController from "./mappings_visualization_controller"
 application.register('mappings', MappingsController)
