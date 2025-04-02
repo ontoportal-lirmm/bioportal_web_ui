b6b14c179e0df26380118cd38e77b62854dc0646
Syphax <gs_bouazzouni@esi.dz>
2025-04-02 21:12:24 +0200

    make the ontology selector larger

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -406,7 +406,7 @@ module ApplicationHelper
     content_tag(:div, class:'insert-sample-text-button') do
       content_tag(:div, class: 'button', 'data-action': 'click->sample-text#annotator_recommender', 'data-sample-text': t("annotator.sample_text")) do
         content_tag(:div, text, class: 'text') +
-        inline_svg_tag('icons/arrow-curved-up.svg')
+          inline_svg_tag('icons/arrow-curved-up.svg')
       end
     end
   end
@@ -415,13 +415,17 @@ module ApplicationHelper
     render Display::EmptyStateComponent.new(text: text)
   end
 
-  def ontologies_selector(id:, label: nil, name: nil, selected: nil, placeholder: nil, multiple: true, ontologies: onts_for_select)
+  def ontologies_selector(id:, label: nil, name: nil, selected: nil, placeholder: nil, multiple: true, ontologies: nil)
+    ontologies ||= onts_for_select
+    selected = Array(selected).map { |x| x.split('/').last }
     content_tag(:div) do
       render(Input::SelectComponent.new(id: id, label: label, name: name, value: ontologies, multiple: multiple, selected: selected, placeholder: placeholder)) +
-      content_tag(:div, class: 'ontologies-selector-button', 'data-controller': 'ontologies-selector', 'data-ontologies-selector-id-value': id) do
-        content_tag(:div, t('ontologies_selector.clear_selection'), class: 'clear-selection', 'data-action': 'click->ontologies-selector#clear') +
-        link_to_modal(t('ontologies_selector.ontologies_advanced_selection'), "/ontologies_selector?id=#{id}", data: { show_modal_title_value: t('ontologies_selector.ontologies_advanced_selection')})
-      end
+        content_tag(:div, class: 'ontologies-selector-button',
+                          'data-controller': 'ontologies-selector', 'data-ontologies-selector-id-value': id) do
+          content_tag(:div, t('ontologies_selector.clear_selection'), class: 'clear-selection', 'data-action': 'click->ontologies-selector#clear') +
+            link_to_modal(t('ontologies_selector.ontologies_advanced_selection'), "/ontologies_selector?id=#{id}",
+                          data: { show_modal_title_value: t('ontologies_selector.ontologies_advanced_selection'), show_modal_size_value: 'modal-xl'})
+        end
     end
   end
 
