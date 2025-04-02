7cfba095d7bcba78781d71427d2f4100bc1d94f9
Syphax <gs_bouazzouni@esi.dz>
2025-04-02 20:55:55 +0200

    fix ontologies selector js code, to debounce the calls

--- app/javascript/controllers/ontologies_selector_controller.js
+++ app/javascript/controllers/ontologies_selector_controller.js
@@ -1,19 +1,24 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ['submit', 'ontology', 'table', 'exit']
+    static targets = ['ontology', 'table', 'exit', 'form', 'frame']
     static values = {
         id: String,
         selectAll: String,
         unselectAll: String
-      }
-    input(){
-        this.submitTarget.click()
     }
-    change(event){
-        this.submitTarget.click()
+
+    input(event) {
+        this.change(event)
+    }
+
+    change(event) {
+        const formData = new FormData(this.formTarget);
+        const url = this.frameTarget.getAttribute('src').split('?')[0];
+        this.frameTarget.src = url + '?' + new URLSearchParams(formData).toString();
         this.#updateTableNumbers(event)
     }
+
     selectall(event) {
         const selectText = `\n${this.selectAllValue}\n`;
         const unselectText = `\n${this.unselectAllValue}\n`;
@@ -24,35 +29,48 @@ export default class extends Controller {
         }
         event.target.innerHTML = newInnerHTML;
     }
+
     apply() {
         const select = document.getElementById(`select_${this.idValue}`);
         const tsControl = document.getElementById(`select_${this.idValue}-ts-control`);
         const values = this.#selectedOntologies(this.ontologyTargets);
         for (const value of values) {
             tsControl.value = value;
-            tsControl.dispatchEvent(new Event('input', { bubbles: true }));
-            select.parentNode.querySelector(`div[data-value="${value}"]`).click();
+            tsControl.dispatchEvent(new Event('input', {bubbles: true}));
+            let selectorVal = select.parentNode.querySelector(`div[data-value="${value}"]`)
+            if (selectorVal) {
+                selectorVal.click();
+            } else {
+                select.tomselect.addOption({value: value.value, text: value.name});
+                select.tomselect.addItem(value.value)
+                selectorVal = select.parentNode.querySelector(`div[data-value="${value.value}"]`)
+                selectorVal.click();
+            }
         }
         this.exitTarget.click();
     }
+
     clear() {
         const selectedItems = document.getElementById(`select_${this.idValue}`).parentNode.querySelectorAll('a');
         selectedItems.forEach(item => {
             item.click();
         });
     }
-    
+
     #selectedOntologies(ontologies) {
         return ontologies
             .filter(ontology => ontology.querySelector('input').checked)
-            .map(ontology => ontology.querySelector('input').name);
+            .map(ontology => {
+                const input = ontology.querySelector('input');
+                return {name: input.name, value: input.value};
+            });
     }
-    
+
     #updateTableNumbers() {
         const navItems = Array.from(this.tableTarget.querySelectorAll('.nav-item'));
         navItems.forEach(item => this.#updateNavItemCount(item));
     }
-    
+
     #updateNavItemCount(navItem) {
         const tabPane = this.tableTarget.querySelector(`.tab-pane${navItem.getAttribute('data-target')}`);
         const inputs = tabPane.querySelectorAll('input');
@@ -68,5 +86,5 @@ export default class extends Controller {
         if (count === 0) {
             itemTitleElement.innerHTML = itemTitle.replace(regex, '').trim();
         }
-    }  
+    }
 }
