2050fb8d875df381d80eb93ec815835cb4cd26b4
AmineBKF <boukerfa.ma@gmail.com>
2025-04-29 17:26:15 +0200

    adapt the datatable component to handle serverside pagination and search

--- app/components/table_component/table_component_controller.js
+++ app/components/table_component/table_component_controller.js
@@ -1,32 +1,62 @@
-import {Controller} from "@hotwired/stimulus"
-import DataTable from 'datatables.net-dt';
+  import { Controller } from "@hotwired/stimulus"
+  import DataTable from 'datatables.net-dt'
 
-
-// Connects to data-controller="table-component"
-export default class extends Controller {
+  // Connects to data-controller="table-component"
+  export default class extends Controller {
     static values = {
-        sortcolumn: String,
-        paging: Boolean,
-        searching: Boolean,
-        noinitsort: Boolean,
-        searchPlaceholder: {type: String, default: 'Filter records'},
+      sortcolumn: String,
+      paging: Boolean,
+      searching: Boolean,
+      noinitsort: Boolean,
+      searchPlaceholder: { type: String, default: 'Search' },
+      serverSide: Boolean,
+      ajaxUrl: String,
+      columns: Array
     }
-    connect(){
-        const table_component = this.element.querySelector('table')
-        const default_sort_column = parseInt(this.sortcolumnValue, 10)
 
-        if (this.sortcolumnValue || this.searchingValue || this.pagingValue){
-            this.table = new DataTable('#'+table_component.id, {
-                paging: this.pagingValue,
-                info: false,
-                searching: this.searchingValue,
-                autoWidth: true,
-                order: this.noinitsortValue ? [] : [[default_sort_column, 'desc']],
-                language: {
-                    search: '_INPUT_',
-                    searchPlaceholder: this.searchPlaceholderValue
-                }
-            });
-        }
+    connect() {
+      const table = this.element.querySelector('table')
+      const defaultSortColumn = parseInt(this.sortcolumnValue, 10)
+
+      if (this.sortcolumnValue || this.searchingValue || this.pagingValue || this.serverSideValue) {
+      
+        this.table = new DataTable(`#${table.id}`, {
+          paging: this.pagingValue,
+          columns: this.columnsValue.map(name => ({ data: name })),
+          info: false,
+          lengthMenu: [
+            [10, 25, 50, 100],
+            [10, 25, 50, 100]
+          ],
+          searching: this.searchingValue,
+          autoWidth: true,
+          serverSide: this.serverSideValue,
+          processing: true,
+          ajax: this.serverSideValue ? {
+            url: this.ajaxUrlValue,
+            data: function (d) {
+              return {
+                page: Math.floor(d.start / d.length) + 1,
+                pagesize: d.length,
+                search: d.search.value 
+              }
+            },
+            dataSrc: function (json) {
+              return json.collection || []
+
+            }
+          } : null,
+          order: this.noinitsortValue ? [] : [[defaultSortColumn, 'desc']],
+          search: {
+            return: true
+          },
+          language: {
+            search: '_INPUT_',
+            searchPlaceholder: this.searchPlaceholderValue
+          }
+        })
+
+        
+      }
     }
-}
+  }
