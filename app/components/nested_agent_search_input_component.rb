90c04eaae9ad0c734c63d96ffb4ae57f0666e0d6
Imad Bourouche <ji_bourouche@esi.dz>
2025-06-27 10:13:41 +0200

    Squashed commit of feature/portal-configuration-interface
    commit 56fdf59ac2e1ea534a0c266a9b2773ba28fffa6f
    Author: Imad Bourouche <ji_bourouche@esi.dz>
    Date:   Fri Jun 27 10:00:41 2025 +0200
    
        fix
    
    commit b6150cc6fb966b7179e574a689d9665a70432722
    Author: Imad Bourouche <ji_bourouche@esi.dz>
    Date:   Fri Jun 27 03:05:26 2025 +0200
    
        Address QA changes

--- app/components/nested_agent_search_input_component.rb
+++ app/components/nested_agent_search_input_component.rb
@@ -4,13 +4,15 @@ class NestedAgentSearchInputComponent < ViewComponent::Base
 
   include Turbo::FramesHelper, AgentHelper
 
-  def initialize(label: nil, agents:, agent_type:, name_prefix:, show_affiliations: true, edit_on_modal: false, parent_id: nil)
+  def initialize(label: nil, agents:, agent_type:, name_prefix:, show_affiliations: true, editable: true, edit_on_modal: false, parent_id: nil, create_new_agent_action: true)
     @agents = agents
     @agent_type = agent_type
     @name_prefix = name_prefix
+    @editable = editable
     @edit_on_modal = edit_on_modal
     @parent_id = parent_id
     @label = label
     @show_affiliations = show_affiliations
+    @create_new_agent_action = create_new_agent_action
   end
 end
