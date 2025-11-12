b96a50c8a8e98efc5087cac97c9ab73d7ba72be1
Mohamed El Amine BOUKERFA <boukerfa.ma@gmail.com>
2025-11-12 17:09:06 +0100

    feat: add sparql endpoint UI

---
+++ app/controllers/sparql_endpoint_controller.rb
@@ -0,0 +1,5 @@
+class SparqlEndpointController < ApplicationController
+  layout :determine_layout
+  def index
+  end
+end
--- app/javascript/controllers/sparql_controller.js
+++ app/javascript/controllers/sparql_controller.js
@@ -3,29 +3,73 @@ import { getYasgui } from '../mixins/useYasgui'
 
 // Connects to data-controller="sparql"
 export default class extends Controller {
-  static  values = {
+  static values = {
     proxy: String,
     username: String,
     apikey: String,
     graph: String,
   }
+  
   connect () {
     localStorage.removeItem('yagui__config');
     this.yasgui = getYasgui(this.element,
       {
-        corsProxy: this.proxyValue,
         copyEndpointOnNewTab: true,
         requestConfig: {
-          endpoint: this.#proxyUrl(),
+          endpoint: this.#proxyUrl(), 
           acceptHeaderGraph: false,
           acceptHeaderUpdate: false,
           namedGraphs: [this.graphValue],
         }
       })
 
+    // Add example query tabs
+    this.#addExampleTabs()
   }
 
   #proxyUrl(){
     return `${this.proxyValue}?apikey=${this.apikeyValue}&username=${this.usernameValue}`
+  }
+
+  #addExampleTabs() {
+    // Wait a bit for YASGUI to fully initialize
+    setTimeout(() => {
+      const queries = [
+        {
+          name: "Discover Classes",
+          query: `SELECT DISTINCT ?class (COUNT(?instance) AS ?instanceCount)
+WHERE {
+  ?instance a ?class .
+}
+GROUP BY ?class
+ORDER BY DESC(?instanceCount) LIMIT 10`
+        },
+        {
+          name: "Datatype Properties", 
+          query: `SELECT DISTINCT ?property ?datatype
+           (COUNT(*) AS ?usageCount)
+WHERE {
+  ?subject ?property ?object .
+  FILTER isLiteral(?object)
+  BIND(datatype(?object) AS ?datatype)
+}
+GROUP BY ?property ?datatype
+ORDER BY DESC(?usageCount)`
+        },
+        {
+          name: "Explore All Triples",
+          query: `SELECT ?subject ?predicate ?object
+WHERE {
+  ?subject ?predicate ?object .
+} LIMIT 50`
+        }
+      ];
+      this.yasgui.getTab().close()
+      for (let i = 0; i < queries.length; i++) {
+        const tab = this.yasgui.addTab(true, { name: queries[i].name}, {atIndex: 0});
+        tab.setQuery(queries[i].query);
+      }
+
+    }, 50); 
   }
 }
