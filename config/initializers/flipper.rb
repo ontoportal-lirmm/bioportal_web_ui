76fea255ad7e11750d82977f35e5d22817e07e50
Mohamed El Amine BOUKERFA <boukerfa.ma@gmail.com>
2025-12-18 13:45:08 +0100

    feat: Add environment banner to Flipper UI

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,2 +1,8 @@
 require Rails.root.join('app/lib/flipper/flipper_setup')
+
+Flipper::UI.configure do |config|
+  config.banner_text = "#{ENV["RAILS_ENV"].capitalize} Environment"
+  config.banner_class = 'danger'
+end
+
 FlipperSetup.configure!
